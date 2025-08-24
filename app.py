df6aba66c449972f06a39221e4574438b14dd9b3
Viviana <ve.zamoras@uea.edu.ec>
2025-08-23 22:49:18 -0500

    Proyecto templates

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Bienvenido a Servi Cool ZA-SA - Tu solución en aires acondicionados.'
+    return render_template('index.html')
 
 @app.route('/acerca')
 def acerca():
-    return 'En Servi Cool ZA-SA nos dedicamos a la venta, instalación y reparación de aires acondicionados.'
+    return render_template('about.html')
 
 @app.route('/contacto')
 def contacto():
-    return 'Llámanos al 0996173679 o escríbenos a servicoolzasa@gmail.com'
+    return render_template('contacto.html')
 
 @app.route('/servicios')
 def servicios():
-    return 'Nuestros servicios: venta de equipos (12, 18 y 24 kBTU), repuestos, instalación y mantenimiento.'
+    return render_template('servicios.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
